dc764433d9fcc564442d86732cfed63701b74057
Nikolay Davydov <davnvl2000@gmail.com>
2019-09-28 20:05:15 +0700

    Header moved from solution to root

--- LorentzVector.h
+++ LorentzVector.h
@@ -1,28 +1,41 @@
+#pragma once
+#include <ostream>
+
 class LorentzVector {
+private:
+	double t;
+	double x;
+	double y;
+	double z;
+
 public:
-    LorentzVector();
-    LorentzVector(double, double, double, double);
+//##########___CONSTRUCTORS___#########################################################################################################
+	LorentzVector();
+	LorentzVector(double input_t, double input_x,\
+				double input_y, double input_z);
+//##########___GETTERS___##############################################################################################################
+	double get_t() const;
+	double get_x() const;
+	double get_y() const;
+	double get_z() const;
+//##########___SETTERS___##############################################################################################################
+	void set_t(double new_t);
+	void set_x(double new_x);
+	void set_y(double new_y);
+	void set_z(double new_z);
+//##########___OPERATIONS___###########################################################################################################
+	double dot(const LorentzVector & other_vector) const;
+	double norm() const;
+//##########___CONVERSION_METHOD___####################################################################################################	
+	void zboost(double beta);		//If |beta| > 1, this function do nothing
+//##########___OPERATORS___############################################################################################################
+	void operator+= (const LorentzVector& other_vector);
+	void operator-= (const LorentzVector& other_vector);
+	void operator*= (const double lambda);
+	LorentzVector operator+ (const LorentzVector& other_vector) const;
+	LorentzVector operator- (const LorentzVector& other_vector) const;
+	LorentzVector operator* (const double lambda) const;
 
-    double t() const;
-    double x() const;
-    double y() const;
-    double z() const;
-    double norm() const;
-
-    void t(double x);
-    void x(double x);
-    void y(double x);
-    void z(double x);
-
-    void operator+=(const LorentzVector& other);
-    void operator-=(const LorentzVector& other);
-    void operator*=(double a);
-    LorentzVector operator+(const LorentzVector& other) const;
-    LorentzVector operator-(const LorentzVector& other) const;
-    friend LorentzVector operator*(const LorentzVector& lv, double a);
-    friend std::ostream& operator << (std::ostream&, const LorentzVector&);
-    void zboost(double beta);
-    double dot(const LorentzVector& other) const;
+	friend LorentzVector operator* (const double lambda, const LorentzVector& l_vector);
+	friend std::ostream& operator<< (std::ostream& std_out_stream, const LorentzVector& l_vector);
 };
-
-std::ostream& operator << (std::ostream&, const LorentzVector&);
